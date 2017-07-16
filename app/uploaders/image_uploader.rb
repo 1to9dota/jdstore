aa88b81b007f16f4fa20f2b439491a211436a620
1to9 <251838931@qq.com>
2017-07-16 08:34:36 +0800

    add

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,9 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
    include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  #storage :file
-  storage :qiniu
-
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
